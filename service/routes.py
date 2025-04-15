dafaae0e4d4cc16bbb261184a04a6aab0871378c
Aditi0815B <167608263+Aditi0815B@users.noreply.github.com>
2025-04-16 00:28:27 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -57,6 +57,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -74,6 +75,7 @@ def list_accounts():
     return jsonify(account_list), status.HTTP_200_OK
 
 
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
@@ -89,6 +91,7 @@ def get_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
+
 
 
 ######################################################################
